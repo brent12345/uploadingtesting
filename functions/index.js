04ed0aea7e07f6e561039c444773261fc11eb87b
brent.morine <brentmorine@gmail.com>
2018-08-16 15:06:24 -0300

    updates

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,17 @@
 const functions = require('firebase-functions');
-const gcs = require('@google-cloud/storage')();
+
 var os = require('os');
 const path = require('path');
 const spawn = require('child-process-promise').spawn;
+const cors = require('cors')({origin: true});
+const Busboy = require('busboy');
+const fs = require('fs');
+// this key needs to be passed into google cloud storage before you can upload files
+const gcconfig ={
+  projectId: "uploadimages-4132e",
+  keyFileName: "uploadimages-4132e-firebase-adminsdk-43wd1-798654bd37"
+}
+const gcs = require('@google-cloud/storage')(gcconfig);
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -48,4 +57,52 @@ exports.onFileChange = functions.storage.object().onFinalize(event => {
 exports.onFileDelete = functions.storage.object().onDelete(event => {
   console.log(event);
   return;
+});
+
+exports.uploadFile = functions.https.onRequest((req, res, event) => {
+  cors(req, res, () => {
+    if (req.method !== 'POST') {
+      return res.status(500).json({
+        message: 'Not Allowed!'
+
+      })
+        }
+        const busboy = new Busboy({headers: req.headers});
+        let uploadData = null;
+        busboy.on("file", (filename, file, encoding, mimetype) => {
+            const filepath = path.join(os.tmpdir(), filename);
+            uploadData = {file: filepath, type: mimetype};
+            file.pipe(fs.createWriteStream(filepath));
+        });
+        //console.log('this is my event event.contentType ' + event.contentType);
+        //console.log('this is my event uploadData.type ' + uploadData.type);
+        busboy.on("finish", () => {
+          const bucket = gcs.bucket("uploadimages-4132e.appspot.com");
+          bucket
+            .upload(uploadData.file, {
+            uploadType: 'media',
+            metadata: {
+              contentType: uploadData.contentType
+            }
+
+        })
+        .then(() => {
+
+          res.status(200).json({
+            message: 'it worked!'
+
+            });
+        })
+        .catch(err => {
+
+           res.status(500).json({
+              error: err
+            });
+
+        });
+
+
+  });
+  busboy.end(req.rawBody);
+  });
 });
